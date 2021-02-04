1edd6a3542b7f6e68f7822bd4087e1d19735fcd0
clairezed <clairezuliani@gmail.com>
2021-02-05 00:03:44 +0100

    Use env var instead of credentials

--- config/initializers/mailjet.rb
+++ config/initializers/mailjet.rb
@@ -1,5 +1,5 @@
 Mailjet.configure do |config|
   config.api_key = ENV['MAILJET_API_KEY']
-  config.secret_key = ENV['MAILJET_SECRET'] 
+  config.secret_key = ENV['MAILJET_SECRET']
   #config.default_from = 'my_registered_mailjet_email@domain.com'
 end
