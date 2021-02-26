e60628fdad83d08d53c27f42a6138ecd59e523ff
clairezed <clairezuliani@gmail.com>
2021-02-26 17:02:17 +0100

    Update style

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,36 @@
-module.exports = {
+let environment = {
   plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-    require('postcss-import'),
-    require('postcss-flexbugs-fixes'),
-    require('postcss-preset-env')({
+    require("tailwindcss"),
+    require("autoprefixer"),
+    require("postcss-import"),
+    require("postcss-flexbugs-fixes"),
+    require("postcss-preset-env")({
       autoprefixer: {
-        flexbox: 'no-2009'
+        flexbox: "no-2009",
       },
-      stage: 3
-    })
-  ]
-}
+      stage: 3,
+    }),
+  ],
+};
+
+// // Only run PurgeCSS in production envrionment, you can also add other environment here
+// if (process.env.RAILS_ENV === "production") {
+//   environment.plugins.push(
+//     require("@fullhuman/postcss-purgecss")({
+//       content: [
+//         "./app/**/*.html.erb",
+//         "./app/**/*.html.haml",
+//         "./app/helpers/**/*.rb",
+//         "./app/javascript/**/*.js",
+//         "./app/javascript/**/*.vue",
+//         "./app/javascript/**/*.jsx",
+//       ],
+//       safelist: {
+//         greedy: [/fa/],
+//       },
+//       defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+//     })
+//   );
+// }
+
+module.exports = environment;
